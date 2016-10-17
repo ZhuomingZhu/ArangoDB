3e34476c6b3f8399cd85c57fb436fce6e94a2079
ZhuomingZhu <zhu2@umbc.edu>
2016-10-17 19:40:26 -0400

    Update create_graph.py

--- create_graph.py
+++ create_graph.py
@@ -10,7 +10,7 @@ client = ArangoClient(
 )
 
  # Create a new graph
- graph = client.db('my_py_database').create_graph('my_graph')
+ graph = client.db('example').create_graph('my_graph')
  students = graph.create_vertex_collection('students')
  courses = graph.create_vertex_collection('courses')
  takes = graph.create_edge_definition(
